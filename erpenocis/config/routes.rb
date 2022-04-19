28e54027e0b913712092c0a9744c0a19cf9b37d5
Alex Rogna <alexrogna@yahoo.com>
2022-04-19 20:06:25 +0100

    Update setting so user can change the password Fixes #21

--- erpenocis/config/routes.rb
+++ erpenocis/config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, controllers:{registrations:'registrations'}
   resources :users
+  get '/settings', to: 'users#settings', as: 'settings'
+  resource :user, only: [:settings] do
+    collection do
+      patch 'update_settings'
+    end
+  end
   root 'dashboard#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
