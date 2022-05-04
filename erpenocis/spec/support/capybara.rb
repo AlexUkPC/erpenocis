f38cd29e579d621b92763a2fb2f31a4bfc77c9f8
Alex Rogna <alexrogna@yahoo.com>
2022-05-04 22:26:21 +0100

    Add capybara, selenium webdriver and set them up Fixes #25

---
+++ erpenocis/spec/support/capybara.rb
@@ -0,0 +1,14 @@
+Capybara.register_driver :selenium_chrome_in_container do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  
+  options.add_argument('--allow-insecure-localhost')
+  options.add_argument('--ignore-certificate-errors')
+	Capybara::Selenium::Driver.new app,
+	browser: :chrome,
+	url: "http://selenium_chrome:4444/wd/hub",
+	capabilities: [options]
+end
+Capybara.default_driver = :selenium_chrome_in_container
+Capybara.server_host = "0.0.0.0"
+Capybara.server_port = 5028
+Capybara.app_host = 'http://web_erpenocis:5028'
