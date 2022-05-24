942874ffefa1d8c95a87d35b296b96e57bfe1b92
Alex Rogna <alexrogna@yahoo.com>
2022-05-24 15:49:12 +0100

    update indirect expenses date range selection

--- erpenocis/app/controllers/indirect_expenses_controller.rb
+++ erpenocis/app/controllers/indirect_expenses_controller.rb
@@ -1,10 +1,6 @@
 class IndirectExpensesController < ApplicationController
+  before_action :set_dates_params, :set_table_head
   def index
-    @start_month = params[:start_month].to_i
-    @start_year = params[:start_year].to_i
-    @end_month = params[:end_month].to_i
-    @end_year = params[:end_year].to_i
-    @months = (@end_year - @start_year) * 12 + @end_month - @start_month + 1
     @employee_salaries = EmployeeSalary.all
     @expense_types = Expense.expense_types.keys
   end
